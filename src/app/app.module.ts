9736b298120c96e0d788a2edeb4164ddc87975c0
samuelcastrok <samcastrok@gmail.com>
2021-07-01 17:32:35 -0300

    sharedmodule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,31 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, LOCALE_ID } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { registerLocaleData } from '@angular/common';
 import { HttpClientModule } from "@angular/common/http";
 import localePt from '@angular/common/locales/pt';
-
-import { InputTextModule } from 'primeng/inputtext';
-import { ButtonModule } from 'primeng/button';
-import { TooltipModule } from "primeng/tooltip";
-import { TableModule } from "primeng/table";
-import { InputTextareaModule } from 'primeng/inputtextarea';
-import { CalendarModule } from "primeng/calendar";
-import { SelectButtonModule } from "primeng/selectbutton";
-import { DropdownModule }  from 'primeng/dropdown';
-import { InputMaskModule }  from 'primeng/inputmask';
-import { InputNumberModule } from 'primeng/inputnumber';
-import { MessagesModule } from 'primeng/messages';
-import { MessageModule } from 'primeng/message';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 
 import { NavbarComponent } from './navbar/navbar.component';
-// import { ITS_JUST_ANGULAR } from '@angular/core/src/r3_symbols';
-import { MessageComponent } from './message/message.component';
 import { LancamentosModule } from './lancamentos/lancamentos.module';
 import { PessoasModule } from './pessoas/pessoas.module';
 
@@ -35,29 +18,12 @@ registerLocaleData(localePt);
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent,
-    MessageComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule,
     HttpClientModule,
-
-    InputTextModule,
-    ButtonModule,
-    TableModule,
-    TooltipModule,
-    InputTextareaModule,
-    CalendarModule,
-    BrowserAnimationsModule,
-    SelectButtonModule,
-    DropdownModule,
-    InputMaskModule,
-    InputNumberModule,
-
-    MessagesModule,
-    MessageModule,
     LancamentosModule,
     PessoasModule
   ],
--- src/app/lancamentos/lancamentos.module.ts
+++ src/app/lancamentos/lancamentos.module.ts
@@ -1,22 +1,24 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
-import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
-import { LancamentoGridComponent } from './lancamento-grid/lancamento-grid.component';
 import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { InputTextModule } from 'primeng/inputtext';
 import { ButtonModule } from 'primeng/button';
 import { TableModule } from 'primeng/table';
 import { TooltipModule } from 'primeng/tooltip';
 import { InputTextareaModule } from 'primeng/inputtextarea';
 import { CalendarModule } from 'primeng/calendar';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SelectButtonModule } from 'primeng/selectbutton';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { InputMaskModule } from 'primeng/inputmask';
 import { DropdownModule } from 'primeng/dropdown';
 import { MessageModule } from 'primeng/message';
+
+import { LancamentoCadastroComponent } from './lancamento-cadastro/lancamento-cadastro.component';
+import { LancamentosPesquisaComponent } from './lancamentos-pesquisa/lancamentos-pesquisa.component';
+import { LancamentoGridComponent } from './lancamento-grid/lancamento-grid.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -38,7 +40,9 @@ import { MessageModule } from 'primeng/message';
     DropdownModule,
     InputMaskModule,
     InputNumberModule,
-    MessageModule
+    MessageModule,
+
+    SharedModule
   ],
   exports: [
     LancamentoCadastroComponent,
